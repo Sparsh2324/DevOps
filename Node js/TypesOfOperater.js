7ed6b3bfd8a2e837e367adc898320600e226d3c8
Sparsh2324 <sandesh.jagtap3@gmail.com>
2025-05-25 19:51:24 +0530

    Node.js & docker file add

---
+++ Node js/TypesOfOperater.js
@@ -0,0 +1,12 @@
+let a="Hello, World";
+let b=30;
+let c=0.0;
+let isStudent=true;
+let score=null;
+let mark
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof isStudent);
+console.log(typeof score);
+console.log(typeof mark);
---
+++ Node js/Variables.js
@@ -0,0 +1,12 @@
+let a="Hello, World";
+let b="30";
+let c="00.00";
+let isStudent=true;
+let score=null;
+let mark
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(isStudent);
+console.log(score);
+console.log(mark);
