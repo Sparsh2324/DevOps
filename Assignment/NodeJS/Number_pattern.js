cee49eb7be8f724a462d7f0486b79322fdd12578
sparsh2324 <sandesh.jagtap3@gmail.com>
2025-06-15 13:34:17 +0530

    NodeJS assignment

---
+++ Assignment/NodeJS/Number_pattern.js
@@ -0,0 +1,11 @@
+console.log("number pattern");
+
+for (let i = 1; i <= 10; i++){
+  if(i % 2 !== 0){
+   let pattern = "";
+    for (let j = 1; j <= i; j++){
+    pattern += j;
+    }console.log(pattern);
+  }else{
+   console.log()}
+}
---
+++ Assignment/NodeJS/Odd_index.js
@@ -0,0 +1,9 @@
+let city = ["Chennai", "Mumbai", "Delhi", "Pune", "Kolkata"];
+
+for (let i = 1; i < city.length; i++) {
+    if (i % 2 != 0) {
+    //if (i % 2 === 0) {
+        console.log(city[i]);
+    }
+ console.log(city.length);
+}
