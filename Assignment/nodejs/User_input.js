2cc6b2f37b9d1105bb70ad93b9c57acdc8744197
sandesh <sandesh.jagtap3@gmail.com>
2025-06-19 17:51:16 +0530

    New files

--- Assignment/nodejs/User_input.js
+++ Assignment/nodejs/User_input.js
@@ -4,7 +4,7 @@ const readline = require("readline").createInterface({
 });
 
 readline.question("Enter a name:", (input1) => {
-  const name1 = parseFloat(input1);
+  const name1 = parseInt(input1);
     console.log(input1);
     readline.close();
 });
--- Assignment/nodejs/even_index.js
+++ Assignment/nodejs/even_index.js
@@ -1,8 +1,8 @@
 let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
 
 for (let i = 0; i < fruits.length; i++) {
-        if (i % 2 === 0){
+        if (i % 2 !== 0){
             console.log(fruits[i]);
             }
-   console.log(fruits.length);
+   //console.log(fruits.length);
 }
