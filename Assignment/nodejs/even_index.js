3241100f9071190752ea7e0c798533770d12ae51
Sparsh2324 <sandesh.jagtap3@gmail.com>
2025-06-22 13:21:14 +0530

    Delete Assignment/nodejs directory

--- Assignment/nodejs/even_index.js
+++
@@ -1,8 +0,0 @@
-let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
-
-for (let i = 0; i < fruits.length; i++) {
-        if (i % 2 === 0){
-            console.log(fruits[i]);
-            }
-   console.log(fruits.length);
-}
